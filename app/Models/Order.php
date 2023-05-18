bb5d31639c5064d81bce9d988a9e89c44e89e2a7
developeruzb <inoyatulla.work@gmail.com>
2023-05-19 02:01:44 +0500

    order show update

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    public function orderItems(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
 }
