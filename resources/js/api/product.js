52ccb62f934a6670950a9bc700f547ba6985e87d
uzsoftic <softuzb@gmail.com>
2023-05-19 10:40:32 +0500

    update

--- resources/js/api/product.js
+++ resources/js/api/product.js
@@ -4,7 +4,14 @@ class product {
 
     // PAGES
     getProducts(data = {}){
+        if(data.query){
+            return http.post('/products/search', data);
+        }
         return http.post('/products', data);
+    }
+
+    searchProducts(data = {}){
+        return http.post('/products/search', data);
     }
 
     getProduct(data = {}){
